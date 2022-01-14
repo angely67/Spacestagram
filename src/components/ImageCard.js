2d8180257b74a4c058bf1d024169e94afc22200b
angely67 <82392932+angely67@users.noreply.github.com>
2022-01-14 15:22:35 -0500

    debugging

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,7 +1,7 @@
 //import '@shopify/polaris/build/esm/styles.css';
 import {Card} from '@shopify/polaris';
 import React from "react";
-//import { RiHeart3Line, RiHeart3Fill } from 'react-icons/ri';
+import { RiHeart3Line, RiHeart3Fill } from 'react-icons/ri';
 
 class ImageCard extends React.Component {
 
@@ -21,17 +21,16 @@ class ImageCard extends React.Component {
     render() {
         return (
             <Card style={{width: "100%", margin: "0", padding: "0"}}>
-             <img src={this.state.img.url} width={"100%"} alt="NASA Images"/>
-                {/*<div style={{padding: "10px"}}>
+             {/*<img src={this.state.img.url} width={"100%"} alt="NASA Images"/>*/}
+                <div style={{padding: "10px"}}>
+                {this.state.img.url}
                 <h1>{this.state.img.title}</h1>
                 <h2>{this.state.img.date}</h2>
                 <text>{this.state.img.explanation}</text>
-                <br/>
-                {/*   
+                <br/> 
          {!this.state.isLiked ? <RiHeart3Line className='heart'style={{color: "red", fontSize: "3em"}} onClick={() => this.likePicture()} /> 
          : <RiHeart3Fill className='heart' style={{color: "red", fontSize: "3em"}} onClick={() => this.likePicture()} />}
-        }
-        </div>*/}
+        </div>
 
       </Card>
         )
