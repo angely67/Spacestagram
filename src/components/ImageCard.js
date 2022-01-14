da6374fc3e79aab961c86e62b94b955921d9d190
angely67 <82392932+angely67@users.noreply.github.com>
2022-01-14 15:12:53 -0500

    debugging application error

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -21,17 +21,17 @@ class ImageCard extends React.Component {
     render() {
         return (
             <Card style={{width: "100%", margin: "0", padding: "0"}}>
-                Hi
-                {this.state.img.url}
-             {/*   <img src={this.state.img.url} width={"100%"} alt="NASA Images"/>
+             <img src={this.state.img.url} width={"100%"} alt="NASA Images"/>
                 <div style={{padding: "10px"}}>
                 <h1>{this.state.img.title}</h1>
                 <h2>{this.state.img.date}</h2>
                 <text>{this.state.img.explanation}</text>
                 <br/>
+                {/*   
          {!this.state.isLiked ? <RiHeart3Line className='heart'style={{color: "red", fontSize: "3em"}} onClick={() => this.likePicture()} /> 
          : <RiHeart3Fill className='heart' style={{color: "red", fontSize: "3em"}} onClick={() => this.likePicture()} />}
-        </div>*/}
+        */}
+        </div>
 
       </Card>
         )
