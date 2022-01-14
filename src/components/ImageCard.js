e6c0698a8994f24d7f7ebe537f12ed8ec04ea023
angely67 <82392932+angely67@users.noreply.github.com>
2022-01-14 14:53:21 -0500

    adding the new parts

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,7 +1,7 @@
 //import '@shopify/polaris/build/esm/styles.css';
 import {Card} from '@shopify/polaris';
 import React from "react";
-//import Heart from "react-animated-heart";
+import { RiHeart3Line, RiHeart3Fill } from 'react-icons/ri';
 
 class ImageCard extends React.Component {
 
@@ -9,21 +9,28 @@ class ImageCard extends React.Component {
         super(props);
 
         this.state = {
-            ImgURL: props.ImgURL,
-            IsLiked: false,
-            MaxWidth: props.MaxWidth
+            img: props.img,
+            isLiked: false
         }
     }
 
+    likePicture = () => {
+        this.setState({isLiked: !this.state.isLiked});
+      }
+
     render() {
         return (
-            <Card style={{width: this.state.MaxWidth, margin: "0", padding: "0"}}>
-                <img src={this.state.ImgURL} width={"100%"} alt="NASA Images"/>
-                   {/*} <Heart isClick={this.state.IsLiked} onClick={() => {
-        this.setState(prevState => ({
-            IsLiked: !prevState.IsLiked
-          }))}
-        } />*/}
+            <Card style={{width: "100%", margin: "0", padding: "0"}}>
+                <img src={this.state.img.url} width={"100%"} alt="NASA Images"/>
+                <div style={{padding: "10px"}}>
+                <h1>{this.state.img.title}</h1>
+                <h2>{this.state.img.date}</h2>
+                <text>{this.state.img.explanation}</text>
+                <br/>
+         {!this.state.isLiked ? <RiHeart3Line className='heart'style={{color: "red", fontSize: "3em"}} onClick={() => this.likePicture()} /> 
+         : <RiHeart3Fill className='heart' style={{color: "red", fontSize: "3em"}} onClick={() => this.likePicture()} />}
+                </div>
+
       </Card>
         )
     }
---
+++ src/components/ImageCardLists.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ImageCard from "./ImageCard";
+
+class ImageCardLists extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            imgs: [],
+            maxWidth: props.MaxWidth
+        }
+    }
+
+    componentDidMount(){
+        fetch("https://api.nasa.gov/planetary/apod?api_key=YL44zoFXxMZ1ZZE0Ig2fxdmV1ieoJmwNa7XOkRdY&count=10") 
+            .then(response => response.json())
+            .then(newImages => {
+                this.setState({imgs: newImages});
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+
+    render() {
+        return (
+            <div style={{width: "50%", marginLeft: "auto", marginRight: "auto"}}>
+                {this.state.imgs.map(d => (
+                    <ImageCard img={d}/>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default ImageCardLists
