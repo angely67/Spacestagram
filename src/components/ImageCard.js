c364d1cf690b935baf24638484eb872af558ff81
angely67 <82392932+angely67@users.noreply.github.com>
2022-01-15 21:56:11 -0500

    add new useful features

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -10,9 +10,18 @@ class ImageCard extends React.Component {
         this.state = {
             img: props.img,
             isLiked: false,
-            isCopied: false
+            isCopied: false,
+            imgOnly: this.props.options.imgOnly,
+            noExplanation: this.props.options.noExplanation
         }
     }
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.options !== this.props.options){
+            this.setState({imgOnly: this.props.options.imgOnly,
+            noExplanation: this.props.options.noExplanation});
+        }
+      }
 
     likePicture = () => {
         this.setState(prevState => ({
@@ -20,26 +29,44 @@ class ImageCard extends React.Component {
           }));
       }
 
-      copyPicture = () => {
+    copyPicture = () => {
         this.setState({isCopied: true});
           navigator.clipboard.writeText(this.state.img.url);
-      }
+    }
 
     render() {
         return (
             <Card style={{width: "100%", margin: "0", padding: "0"}}>
                 <img src={this.state.img.url} width="100%" alt="NASA Images"></img>
+                
                 <div style={{padding: "10px"}}>
+                {!this.state.imgOnly ?
+                <span>
                     <h1>{this.state.img.title}</h1>
                     <h2>{this.state.img.date}</h2>
+                    {!this.state.noExplanation?
                     <span style={{fontSize: "1.2em"}}>{this.state.img.explanation}</span>
+                    :null}
                     <br/>
-                    {!this.state.isLiked ? <RiHeartLine title="Please Like Me :)" style={{color: "red", fontSize: "3em", cursor: "pointer"}} onClick={() => this.likePicture()} /> 
-                    : <RiHeartFill title="Please Don't Dislike Me :(" style={{color: "red", fontSize: "3em", cursor: "pointer"}} onClick={() => this.likePicture()} />}
+                    </span>
+                    : null}
+                    {!this.state.isLiked ? 
+                    <RiHeartLine title="Please Like Me :)" 
+                    style={{color: "red", fontSize: "3em", cursor: "pointer"}} 
+                    onClick={() => this.likePicture()} /> 
+                    : <RiHeartFill title="Please Don't Dislike Me :(" 
+                    style={{color: "red", fontSize: "3em", cursor: "pointer"}} 
+                    onClick={() => this.likePicture()} />}
 
-                    {!this.state.isCopied ? <RiFileCopyLine title="Copy Image URL to Clipboard" style={{color: "black", fontSize: "3em", cursor: "pointer"}} onClick={() => this.copyPicture()} /> 
-                    : <RiFileCopyFill title="Copy Image URL to Clipboard" style={{color: "green", fontSize: "3em", cursor: "pointer"}} onClick={() => this.copyPicture()} />}
+                    {!this.state.isCopied ? 
+                    <RiFileCopyLine title="Copy Image URL to Clipboard" 
+                    style={{color: "black", fontSize: "3em", cursor: "pointer"}} 
+                    onClick={() => this.copyPicture()} /> 
+                    : <RiFileCopyFill title="Copy Image URL to Clipboard" 
+                    style={{color: "green", fontSize: "3em", cursor: "pointer"}} 
+                    onClick={() => this.copyPicture()} />}
                 </div>
+                
             </Card>
         )
     }
